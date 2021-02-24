b93addb0187dde17680ec7ebb9d544b41d38b3d1
Daryl Smith <daryl1976@gmail.com>
2021-02-23 22:07:06 -0500

    Added formulas and refactored

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,78 +8,94 @@ import * as jexcel from 'jexcel';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent  implements OnInit, AfterViewInit  {
+  
+  //this adds the JSpreadsheet Element
   @ViewChild('spreadsheet') spreadsheet: ElementRef;
-  title = 'angular-sheets';
-  spreadBackColor = "aliceblue";
-  sheetName = "Goods List";
-  hostStyle = {
+  
+  //styling for SpreadJS
+  public SpreadJSWidth=100;
+  public title = 'angular-sheets';
+  public spreadBackColor = "aliceblue";
+  public sheetName = "Sleep";
+  public hostStyle = {
     width: "800px",
     height: "600px",
   };
 
-  private jSpreadheetData:any = [
-    ['Mazda', 2001, 2000],
-    ['Pegeout', 2010, 5000],
-    ['Honda Fit', 2009, 3000],
-    ['Honda CRV', 2010, 6000],
-];
-  data = [
+
+  private daysOfTheWeek=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+
+  //this holds the data for spreadJs and SpreadsheetJS
+  public SpreadJsData =[];
+  public JSpreadsheetData=[];
+
+  ngOnInit(): void {
+
+    //set up the data arrays for each
+    this.daysOfTheWeek.forEach((day:string,index:number)=>{
+
+      this.SpreadJsData.push(
         {
-      Name: "Apple",
-      Category: "Fruit",
-      Price: 1,
-      "Shopping Place": "Wal-Mart",
-    },
-    {
-      Name: "Potato",
-      Category: "Fruit",
-      Price: 2.01,
-      "Shopping Place": "Other",
-    },
-    {
-      Name: "Tomato",
-      Category: "Vegetable",
-      Price: 3.21,
-      "Shopping Place": "Other",
-    },
-    {
-      Name: "Sandwich",
-      Category: "Food",
-      Price: 2,
-      "Shopping Place": "Wal-Mart",
-    },
-    {
-      Name: "Hamburger",
-      Category: "Food",
-      Price: 2,
-      "Shopping Place": "Wal-Mart",
-    },
-    {
-      Name: "Grape",
-      Category: "Fruit",
-      Price: 4,
-      "Shopping Place": "Sun Store",
-    },
-  ];
-  columnWidth = 100;
-  
+          Day: day,
+          HoursInBed:0,
+          HoursAsleep:0,
+        }
+      );
+     
+
+      this.JSpreadsheetData.push(
+        [day,'01/01/2021',0,0,`=(D${index}/C${index})*100`]
+      );
+
+
+    });
+
+
+  }
+
+
+  // set the dropdowns for SpeadJS
   workbookInit(args) {
+    let style = new GC.Spread.Sheets.Style();
+    style.cellButtons = [
+        {
+            imageType: GC.Spread.Sheets.ButtonImageType.dropdown,
+            command: "openDateTimePicker",
+            useButtonStyle: true,
+        }
+    ];
+    style.dropDowns = [
+        {
+            type: GC.Spread.Sheets.DropDownType.dateTimePicker,
+             option: {
+                showTime: true,
+             }
+        }
+      ];
+
     let spread: GC.Spread.Sheets.Workbook = args.spread;
     let sheet = spread.getActiveSheet();
-    sheet.getCell(0, 0).text("My SpreadJS Angular Project").foreColor("blue");
+
+    for(let i=0;i<this.daysOfTheWeek.length;i++ ){
+    sheet.setStyle(i, 1, style);
+    sheet.setFormula(i,4,`=(D${i+1}/C${i+1})*100`)
+    spread.commandManager().execute({ cmd: "openDateTimePicker", row: i, col: 1, sheetName: "Sheet1" });
+    }
 
 
 
   }
-  ngOnInit(): void {
-  }
+
+
   ngAfterViewInit() {
     jexcel(this.spreadsheet.nativeElement, {
-      data: this.jSpreadheetData,
+      data: this.JSpreadsheetData,
         columns: [
-          { title: 'Model', width: 300 },
-          { title: 'Price', width: 80 },
-          { title: 'Model', width: 100 }
+          { title: 'Day', width: 300 },
+          { title: 'Date', width: 300, type: 'calendar', options: { format:'DD/MM/YYYY', time:0 } },
+          { title: 'HoursInBed', width: 80,type: 'numeric'  },
+          { title: 'HoursAsleep', width: 100,type: 'numeric' },
+          { title:'Sleep Efficiency', type: 'text',  width:'100' },
       ],
       minDimensions: [10, 10]
     });
